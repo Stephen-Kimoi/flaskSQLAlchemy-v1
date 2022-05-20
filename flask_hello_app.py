0039a4512faa172963a694128fe03a92ea49bdec
Steve Kimoi <moenisteve@gmail.com>
2022-05-20 11:51:24 +0300

    reading data from terminal using dunder repper

--- flask_hello_app.py
+++ flask_hello_app.py
@@ -2,13 +2,17 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__) 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:abc@localhost:5432/flask-app-v1'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:abc@localhost:5432/flask-app-v1' 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
 db = SQLAlchemy(app)
 
 class Persons(db.Model): 
     ___tablename___ = 'persons' 
     id = db.Column(db.Integer, primary_key=True) 
-    name = db.Column(db.String(), nullable=False) 
+    name = db.Column(db.String(), nullable=False)  
+
+    def __repr__(self): 
+        return f'<Person ID:{self.id} name:{self.name}>'
 
 db.create_all() 
 
